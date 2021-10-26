6fec9a32de1ea6cf1b640950e6a0332b89769151
axyst <axysta@gmail.com>
2021-10-26 23:41:47 +0800

    update to exported version

--- DodgeTheCreeps/gd/Player.gd
+++ DodgeTheCreeps/gd/Player.gd
@@ -1,27 +1,44 @@
 extends Area2D
+
 signal hit
 
-export var speed = 400  # How fast the player will move (pixels/sec).
-var screen_size  # Size of the game window.
+export var speed = 400
+var screen_size
+# Add this variable to hold the clicked position.
+var target = Vector2()
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size
 	hide()
+	screen_size = get_viewport_rect().size
 
+func start(pos):
+	position = pos
+	# Initial target is the start position.
+	target = pos
+	show()
+	$CollisionShape2D.disabled = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Change the target whenever a touch event happens.
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+
 func _process(delta):
-	var velocity = Vector2()  # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	var velocity = Vector2()
+	# Move towards the target and stop when close.
+	if position.distance_to(target) > 10:
+		velocity = target - position
+
+# Remove keyboard controls.
+#    if Input.is_action_pressed("ui_right"):
+#       velocity.x += 1
+#    if Input.is_action_pressed("ui_left"):
+#        velocity.x -= 1
+#    if Input.is_action_pressed("ui_down"):
+#        velocity.y += 1
+#    if Input.is_action_pressed("ui_up"):
+#        velocity.y -= 1
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -29,26 +46,22 @@ func _process(delta):
 		$AnimatedSprite.stop()
 
 	position += velocity * delta
+	# We still need to clamp the player's position here because on devices that don't
+	# match your game's aspect ratio, Godot will try to maintain it as much as possible
+	# by creating black borders, if necessary.
+	# Without clamp(), the player would be able to move under those borders.
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_v = false
-		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
-
-func _on_Player_body_entered(body):
-	hide()  # Player disappears after being hit.
+func _on_Player_body_entered( body ):
+	hide()
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
-
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
